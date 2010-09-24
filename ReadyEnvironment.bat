41a4758473f39bddc90ecbf0f196904f1ab0377b
Tim Simpson <timsimpson4@gmail.com>
2010-09-24 05:34:35 +0000

    Just messing with Build Bot #2

--- ReadyEnvironment.bat
+++ ReadyEnvironment.bat
@@ -1,6 +1,5 @@
 "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-PATH=%PATH%;F:\Lp3\Projects\Macaroni\Code\trunk\Macaroni\Macaroni\Debug
- 
+PATH=%PATH%;F:\Lp3\Projects\Macaroni\Code\trunk\Macaroni\Macaroni\Debug 
 
 
 
