b6fc845c0b26aa9ce4d423ebd860a4e36b4d2c5e
Tim Simpson <timsimpson4@gmail.com>
2010-12-11 18:37:51 +0000

    Deleting obsolete build files and scripts.

--- Main/App/PrepDebug.bat
+++
@@ -1,8 +0,0 @@
-mkdir "Debug"
-mkdir "Debug\Compilers"
-xcopy "Source\Compilers" ".\Debug\Compilers" /y /s /e
-mkdir "Debug\Generators"
-xcopy "Source\Generators" ".\Debug\Generators" /y /s /e
-xcopy "Source\Messages.txt" ".\Debug\Messages.txt" /y /s /e
-"C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-PATH=%PATH%;F:\Lp3\Projects\Macaroni\Code\trunk\Macaroni\Macaroni\Debug
