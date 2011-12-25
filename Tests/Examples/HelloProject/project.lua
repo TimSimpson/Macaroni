68e16bc9492c527411f6aecc8bd9d8af5cb68f07
Tim Simpson <timsimpson4@gmail.com>
2011-12-25 13:07:52 -0600

    Preliminary work on putting plugins in the Project system.

--- Tests/Examples/HelloProject/project.lua
+++ Tests/Examples/HelloProject/project.lua
@@ -14,8 +14,49 @@ lib = project:Library(
     { FileSet.Create("src/main/mcpp", ".mcpp$") }
 )
 
+local bjam = plugins:Get("bjam")
+local cpp = plugins:Get("Cpp")
+
+local buildContext = BuildContext("target")
+cpp:Run("Generate", lib, buildContext, {})
+
+
+bjam:Run("Generate", lib, buildContext, {
+
+})
+
+local output = "target"
+
+lib:Generate("Cpp", {output=output})
+
+-- plugins as PluginFactory
+--
+
+local builder = BuildContext(lib, output)
+
+cpp:Run("Generate", lib, { output=output} )
+
+-- I like this style the best:
+lib:Generate("Cpp", {output=output})
+
+-- But this style seems more flexible
+
+-- Properties is automatically added.
+--
+
+
+
+-- plugins stores AppPaths
+
+cpp.Run("Generate", lib, {output=output})
+
 print("Library:")
 print(lib)
+
+
+function install()
+
+end
 
 -- In the future try it this way:
 -- lib = project:Library
