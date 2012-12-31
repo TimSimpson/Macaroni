693b6c8b4273755b08f35b164c5230421c658c9b
Tim Simpson <timsimpson4@gmail.com>
2012-12-31 14:47:11 -0600

    Changed "void main" to "int main" in several spots.

--- Next/Tests/Features/ConstructorOverloading/Source/Main.cpp
+++ Next/Tests/Features/ConstructorOverloading/Source/Main.cpp
@@ -1,11 +1,11 @@
 
 #include "Amigo.h"
 
-void main(int argc, const char * argv[])
+int main(int argc, const char * argv[])
 {
 		Amigo one;
 		Amigo two(255, 255);
-		
+
 		Amigo three = two;
-		
+		return 0;
 }
