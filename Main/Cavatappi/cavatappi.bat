21a7a6cd9b41f60d185e11f8b6108416745b4141
Tim Simpson <timsimpson4@gmail.com>
2014-04-13 10:09:20 -0500

    Cleaned up command line interface 123456789012345678901234567890123456789012345678901234567890123456789012 Switch from hand-cranked code to boost::program_options which has made things much nicer. While I was at it I also created a few more command line switches and tried to make problem areas more sane.
    * There's now an actual help message. Can you believe it! The future
      has truly arrived.
    * Messages is now cleaned up. It only expects to ever have init called
      once, which must happen before anything else occurs in the program.
    * The question of whether or not to catch all exceptions was being
      handled by a compile time setting (debug mode would always throw).
      I've hit quite a few situations where getting the program to abort
      is a major hassle so I made this governed by a command line switch
      instead (--debug).
    * The times are no longer printed out next to all console output
      unless --printTimes is specified.
    * Macaroni used to always print out its paths. Now it does no such
      thing unless "--showPaths" is specified.
    * It's possible now to pass a path as the project file to be
      executed.

--- Main/Cavatappi/cavatappi.bat
+++ Main/Cavatappi/cavatappi.bat
@@ -1,18 +1,4 @@
 @echo off
-REM If built with Boost build, use this:
-REM %CAVATAPPI%\..\App\GeneratedSource\release\debug\macaroni generatorPath %CAVATAPPI%\..\Generators libraryRepoPath %CAVATAPPI%\LibrariesClone cavatappi %*
-REM If built with VS2008, use this:
-REM %CAVATAPPI%\..\App\Debug\Macaroni.App generatorPath %CAVATAPPI%\..\Generators libraryRepoPath %CAVATAPPI%\..\Libraries messagesPath %CAVATAPPI%\..\App\Source\main\resources cavatappi %*
-REM If built with VS11, use this:
 SET GEN_PATH=%CAVATAPPI%\..\Generators
-echo GEN_PATH=%GEN_PATH%
-REM dir %GEN_PATH%
-REM %CAVATAPPI%\..\App\Debug\MacaroniVS11.exe generatorPath %GEN_PATH% libraryRepoPath %CAVATAPPI%\..\Libraries messagesPath %CAVATAPPI%\..\App\Source\main\resources cavatappi %*
-
-REM %CAVATAPPI%\..\App\GeneratedSource\exe\macaroni.exe generatorPath %GEN_PATH% libraryRepoPath %CAVATAPPI%\..\Libraries messagesPath %CAVATAPPI%\..\App\Source\main\resources cavatappi %*
-
-
-
-REM %CAVATAPPI%\..\App\GeneratedSource\bin\gcc-mingw-4.7.2\debug\link-static\threading-multi\macaroni.exe generatorPath %GEN_PATH% libraryRepoPath %CAVATAPPI%\..\Libraries messagesPath %CAVATAPPI%\..\App\Source\main\resources cavatappi %*
 @echo on
-%CAVATAPPI%\..\App\GeneratedSource\exe\macaroni.exe --generatorPath %GEN_PATH% --libraryRepoPath %CAVATAPPI%\..\Libraries --messagesPath %CAVATAPPI%\..\App\Source\main\resources cavatappi %*
+%CAVATAPPI%\..\App\GeneratedSource\exe\macaroni.exe --generatorPath=%GEN_PATH% --libraryRepoPath=%CAVATAPPI%\..\Libraries --messagesPath=%CAVATAPPI%\..\App\Source\main\resources\Messages.txt %*
