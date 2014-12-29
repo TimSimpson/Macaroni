6976bfca9e5fb79fed017e1b9e7654805cb99448
Tim Simpson <timsimpson4@gmail.com>
2014-12-29 12:06:39 -0600

    Adds missing return statements
    Looked for all bugs like this and fixed them.

--- Main/App/Source/main/mcpp/Macaroni/Model/ElementLua.cpp
+++ Main/App/Source/main/mcpp/Macaroni/Model/ElementLua.cpp
@@ -95,8 +95,8 @@
 		}
 		else
 		{
-			luaL_error(L, "Expected a Target or (old-style) Library for "
-				          "argument 2.");
+			return luaL_error(L, "Expected a Target or (old-style) Library for "
+				                 "argument 2.");
 		}
 
 		LUA_GLUE_CATCH
