e9a3e6e4a2fcfdf377395da0e716ad4824c1279f
Tim Simpson <timsimpson4@gmail.com>
2015-05-21 01:15:57 -0500

    Stupid build script tricks

--- Main/App/Build.bat
+++ Main/App/Build.bat
@@ -1,3 +1,4 @@
+@echo off
 SET LIBRARY_PATH=C:\Users\Tim\Tools\Local\Boost\boost_1_52_0\stage\lib
 
 REM Can't figure out how to make this work without supplying the suffix.
@@ -5,4 +6,6 @@ REM SET BOOST_LIB_SUFFIX=-mgw47-mt-1_52
 REM bjam -d+2 -j8 --toolset=gcc cxxflags=-std=gnu++11 link=static threading=multi
 REM bin\gcc-mingw-4.7.2\debug\link-static\threading-multi\macaroni.exe luaTests Source\test\lua
 
-macaroni --messagesPath=Source\main\resources\Messages.txt --libraryRepoPath=..\Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
+REM macaroni --messagesPath=Source\main\resources\Messages.txt --libraryRepoPath=..\Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
+echo WHY
+bash build.sh %*
---
+++ Main/App/Build.sh
@@ -0,0 +1,10 @@
+set -e
+
+fail_message() {
+    bad.sh
+}
+
+trap " fail_message " INT TERM EXIT
+macaroni --messagesPath=Source/main/resources/Messages.txt --libraryRepoPath=../Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
+trap - INT TERM EXIT
+good.sh
