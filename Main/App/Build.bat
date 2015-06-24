d364cf6c7882b5295de83a94b440478537d9f7c5
Tim Simpson <timsimpson4@gmail.com>
2015-06-23 23:21:33 -0500

    Fixing up build script
    This isn't working as portably as I thought it would have...

--- Main/App/Build.bat
+++ Main/App/Build.bat
@@ -6,6 +6,6 @@ REM SET BOOST_LIB_SUFFIX=-mgw47-mt-1_52
 REM bjam -d+2 -j8 --toolset=gcc cxxflags=-std=gnu++11 link=static threading=multi
 REM bin\gcc-mingw-4.7.2\debug\link-static\threading-multi\macaroni.exe luaTests Source\test\lua
 
-REM macaroni --messagesPath=Source\main\resources\Messages.txt --libraryRepoPath=..\Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
+macaroni --messagesPath Source\main\resources\Messages.txt --libraryRepoPath=..\Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
 echo WHY
-bash build.sh %*
+REM bash build.sh %*
--- Main/App/Build.sh
+++ Main/App/Build.sh
@@ -5,6 +5,6 @@ fail_message() {
 }
 
 trap " fail_message " INT TERM EXIT
-macaroni --messagesPath=Source/main/resources/Messages.txt --libraryRepoPath=../Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
+macaroni --messagesPath=Source/main/resources --libraryRepoPath=../Libraries  -b  --showPaths -o "link=static --toolset=msvc-12.0 -j8" --printTimes --verbose
 trap - INT TERM EXIT
 good.sh
