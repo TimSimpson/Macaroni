b4465e81d043189592822e266b604590d6c45943
Tim Simpson <timsimpson4@gmail.com>
2016-05-29 09:16:16 -0500

    Bump version to 0.4.2

--- Next/Release/project.lua
+++ Next/Release/project.lua
@@ -9,7 +9,7 @@ local Path = require "Macaroni.IO.Path";
 sources = { "source/scripts", "source/www" }
 outputPath = "target"
 
-local version = "0.4.1"
+local version = "0.4.2"
 project = context:Group("Macaroni")
                  :Project("Macaroni.Release")
                  :Version(version)
