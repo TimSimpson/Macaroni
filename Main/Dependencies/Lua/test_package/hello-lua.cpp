aefed7cc9b4f50c1b69b039e96fad04f0cbd95db
Tim Simpson <timsimpson4@gmail.com>
2016-06-15 23:45:44 -0500

    Fixes Lua module to compile C++

--- Main/Dependencies/Lua/test_package/hello-lua.cpp
+++ Main/Dependencies/Lua/test_package/hello-lua.cpp
@@ -1,6 +1,24 @@
-#include "lua.h"
+#include <iostream>
+#include <string>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 
-int main (void){
-    lua_State * L;
 
+const std::string LUA_CODE = "require 'os'; print('Hello Moon')";
+
+
+int main() {
+    lua_State * L = luaL_newstate();
+    luaL_openlibs(L);
+    int error = luaL_loadbuffer(L, LUA_CODE.c_str(), LUA_CODE.size(),
+                                "hello-lua.cpp")
+                || lua_pcall(L, 0, 0, 0);
+    if (error)
+    {
+        std::cerr << "An error occured within Lua:"
+            << lua_tostring(L, -1) << std::endl;
+    }
+    lua_close(L);
+    return error;
 }
